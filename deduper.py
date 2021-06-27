9f36f89bbedb7f25783f2f79f5d0b2f2e45d8d47
Anish Niranjan Hemmady <anishniranjan.hemmady@sophos.com>
2021-06-26 23:43:01 -0400

    Added some tests and modified code

--- deduper.py
+++ deduper.py
@@ -1,11 +1,11 @@
-from typing import Callable, Generator
+from typing import Callable, Generator, Union, Tuple, KeysView, ValuesView
 
 
 class Deduper:
     name = "Dedups list of dictionaries based on key criteria"
 
     @classmethod
-    def dedup_data(cls, data: list, func: Callable, matched_data_req=False) -> Generator:
+    def async_dedup_data(cls, data: list, func: Callable, matched_data_req=False) -> Generator:
 
         uniq_set = set()
         uniq_set_add = uniq_set.add
@@ -16,4 +16,13 @@ class Deduper:
                 uniq_set_add(field_val)
                 yield (field_val, element) if matched_data_req else field_val
 
+    @classmethod
+    def sync_dedup_data(cls, data: list, func: Callable, matched_data_req=False) -> Union[Tuple[KeysView, ValuesView], KeysView]:
+        uniq_map = {}
 
+        for element in data:
+            field_val = func(element)
+            if field_val not in uniq_map:
+                uniq_map[field_val] = element
+
+        return (uniq_map.keys(), uniq_map.values()) if matched_data_req else uniq_map.keys()
--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,6 +1,19 @@
+import timeit
+
+num_of_maps = 1000
+setup = '''
+import random
+from deduper import Deduper
+num_of_maps = 1000
 dedup_me = [{"name": "anish", "val": 12}, {"name": "rahul", "val": 17}, {"name": "anish", "val": 12},
             {"name": "anish", "val": 12}, {"name": "rahul", "val": 17}, {"name": "rahulp", "val": 19}]
+big_list = [random.choice(dedup_me) for _ in range(0, num_of_maps)]
+'''
+number = 1000
+t = timeit.Timer(stmt='list(Deduper.async_dedup_data(big_list,lambda x: (x.get("name"),x.get("val"))))', setup=setup)
+avg_time = t.timeit(number)
+single_run_time = avg_time/number
+print(f'Time it took to run {number} of times having {num_of_maps} of dicts in list: {avg_time}')
+print(f'Time it took to run 1 time having {num_of_maps} of dicts in list: {single_run_time}')
 
-big_list = [random.choice(dedup_me) for _ in range(0, 1000000)]
-dedup_gen = Deduper.dedup_data(big_list, lambda x: (x.get("name")))
-res = [r for r in dedup_gen]
+
